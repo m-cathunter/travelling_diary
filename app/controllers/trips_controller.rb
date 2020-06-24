bed0dfeb31a4f6213ee09f5a880ce96bf427e6b2
Magdalena Kotołowska <m.kotolowska@gmail.com>
2020-06-24 12:29:28 +0200

    Add create action to trip controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,5 +3,23 @@ class TripsController < ApplicationController
   end
 
   def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+
+    if @trip.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:city)
   end
 end
