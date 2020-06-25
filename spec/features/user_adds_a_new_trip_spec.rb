e23aa2d7fc4ecd89b22fa8cfc3c2375f4bc6819f
Magdalena Kotołowska <m.kotolowska@gmail.com>
2020-06-25 23:15:41 +0200

    Fix bug in rspec test

--- spec/features/user_adds_a_new_trip_spec.rb
+++ spec/features/user_adds_a_new_trip_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.feature "User adds a new post" do
   scenario "and see the page with submitted city" do
-    trip_city = "My new work"
+    trip_city = "Warsaw"
 
     visit root_path
     click_on "Add new trip"
